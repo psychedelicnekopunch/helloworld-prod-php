6d42100b82a782a318a08e09ab96bea800a30508
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2024-10-09 15:59:07 +0900

    local

---
+++ app/public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>helloworld-prod-php</title>
+	</head>
+	<body>
+		<h1>hello world!</h1>
+		<p><a href="index.html">index.html &gt;&gt;</a></p>
+		<hr>
+		<?php phpinfo(); ?>
+	</body>
+</html>
